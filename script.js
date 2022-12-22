21541b8688605e48a00d0764e0b3a8230f0a1a44
Maya Lynn <mayalynncohen@gmail.com>
2022-12-22 15:00:52 -0800

    Fixed bugs and finalized js code

--- script.js
+++ script.js
@@ -2,31 +2,31 @@ var numbers = "0123456789";
 var lowCase = "abcdefghijklmnopqrstuvwxyz";
 var Upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-var passwordLength;
-var optionPool = "";
-var randomize = [];
 
-
-// askLength();
 function generatePassword() {
+  var passwordLength = 0;
+  var optionPool = "";
   var length = prompt("Please enter the desired password length between 8 and 128", "");
-  if (length >= 8 && length <= 128) {
+  if (length < 8 || length > 128) {
+    alert("Please input a number between 8 and 128!");
+    return generatePassword();
+  } else {
     passwordLength = length;
     if (confirm("Do you want your password to contain numbers?")) {
       optionPool = optionPool + numbers;
     } else {
       optionPool = optionPool + "";
-    } 
+    }
     if (confirm("Do you want your password to contain lowercase letters?")) {
       optionPool = optionPool + lowCase;
     } else {
       optionPool = optionPool + "";
-    }  
+    }
     if (confirm("Do you want your password to contain upercase letters?")) {
       optionPool = optionPool + Upcase;
     } else {
       optionPool = optionPool + "";
-    } 
+    }
     if (confirm("Do you want your password to contain special characters?")) {
       optionPool = optionPool + specialCharacters;
 
@@ -35,29 +35,21 @@ function generatePassword() {
     }
     if (optionPool == "") {
       alert("You did not choose any characters for your password, please try again");
-      generatePassword();
+      return generatePassword();
     } else {
-      for (let i = 0; i < passwordLength; i++) {
-        randomize.push(optionPool[Math.floor(Math.random() * optionPool.length)])
-      };
-      return randomize.join('');
+      return randomizePassword(passwordLength, optionPool);
     }
-  } else if (length < 8) {
-    alert("Must input a number greater or equal to 8")
-    generatePassword();
   }
 }
 
-
-// var randomize = []
-
-// for (let i = 0; i < passwordLength; i++) {
-//   randomize.push(optionPool[Math.floor(Math.random() * optionPool.length)])
-// }
-
-// function generatePassword() {
-//   return randomize.join('')
-// }
+// Randomizer
+function randomizePassword(x, y) {
+  var randomize = [];
+  for (let i = 0; i < x ; i++) {
+    randomize.push(y[Math.floor(Math.random() * y.length)])
+  };
+  return randomize.join('');
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
